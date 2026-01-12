b8b5d3ac67e74911457fc270a692683272b6ba61
iammdsufiyan <uaremdsufian@gmail.com>
2026-01-13 03:04:57 +0530

    jav

--- Desktop/java/recursion/howToTakeInut.java
+++ Desktop/java/recursion/howToTakeInut.java
@@ -2,15 +2,26 @@ import java.util.Scanner;
 
 public class howToTakeInut {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
+       String str = sc.nextLine();
 
-        String str = sc.nextLine();
-      String [] parts=  str.split(",|\\s+");
+       String []  paths = str.split(",|\\s+");
 
-      for(int i=0;i<parts.length; i++){
-    
-        int num = Integer.parseInt(parts[i]);
-        System.out.println(num);
-      }
+        for(int i=0;i<paths.length;i++){
+            int x = Integer.parseInt(paths[i]);
+            System.out.print(x);
+        }
     }
 }
+
+
+//  Scanner sc = new Scanner(System.in);
+
+//         String str = sc.nextLine();
+//       String [] parts=  str.split(",|\\s+");
+
+//       for(int i=0;i<parts.length; i++){
+    
+//         int num = Integer.parseInt(parts[i]);
+//         System.out.println(num);
+//       }

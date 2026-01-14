ee8925064caf8a9094a8e389fabb4df4881dd99e
iammdsufiyan <uaremdsufian@gmail.com>
2026-01-14 21:50:06 +0530

    recur

--- Desktop/java/recursion/howToTakeInut.java
+++ Desktop/java/recursion/howToTakeInut.java
@@ -16,12 +16,3 @@ public class howToTakeInut {
 
 
 //  Scanner sc = new Scanner(System.in);
-
-//         String str = sc.nextLine();
-//       String [] parts=  str.split(",|\\s+");
-
-//       for(int i=0;i<parts.length; i++){
-    
-//         int num = Integer.parseInt(parts[i]);
-//         System.out.println(num);
-//       }
